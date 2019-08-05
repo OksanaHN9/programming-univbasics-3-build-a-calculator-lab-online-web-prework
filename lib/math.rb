756f4c8b10ee8001b1ff1ddf9d436c70ba0e0e4a
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-05 01:31:09 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
 def addition(num1, num2)
-
-end
+addition =
+end return addition
 
 def subtraction(num1, num2)
-
+substraction =
 end
 
 def division(num1, num2)
-
+division =
 end
 
 def multiplication(num1, num2)
-
+multiplication =
 end
 
 def modulo(num1, num2)
-
+modulo =
 end
 
 def square_root(num)
-
+square_root =
 end
+num1 = 3
+num2 = 4
+addition = num1 + num2
